6a7a3eb2d942af5aa38b042a69e3f907dde72b8d
Dean jakovlevski <dean.jakovlevski@seavus.com>
2017-05-02 15:48:34 +0200

    added api text examples

--- FiktFinanceApi/Controllers/HomeController.cs
+++ FiktFinanceApi/Controllers/HomeController.cs
@@ -1,18 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FiktFinanceApi.Models;
 
 namespace FiktFinanceApi.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
-        public ActionResult Index()
+        [HttpGet]
+        public TestResponse GetData(int parametarId)
         {
-            ViewBag.Title = "Home Page";
-
-            return View();
+            var conn = ConfigurationManager.ConnectionStrings[GetConnectionStringName()].ConnectionString;
+            var response = new TestResponse
+            {
+                Status = ResponseStatus.Success,
+            };
+            try
+            {
+                using (var con = new SqlConnection(conn))
+                {
+                    var command = new SqlCommand("ime na procedurata", con) { CommandType = CommandType.StoredProcedure };
+                    command.Parameters.AddWithValue("ime na oarametarot", parametarId);
+                    con.Open();
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var test = new TestResponse
+                        {
+                            Id = Convert.ToInt32(reader["ime na parametarot"]),
+                            Name = reader["ime na prametarot"].ToString(),
+                        };
+                        response.Id = test.Id;
+                        response.Name = test.Name;
+                    }
+                    con.Close();
+                }
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Status = ResponseStatus.Error;
+                response.Message = "RequestFailed";
+                return response;
+            }
         }
     }
 }
---
+++ FiktFinanceApi/Models/BaseResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FiktFinanceApi.Models
+{
+    /// <summary>
+    /// Base response class
+    /// </summary>
+    public class BaseResponse
+    {
+        /// <summary>
+        /// Base message returned to client
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// Response status
+        /// </summary>
+        public ResponseStatus Status { get; set; }
+    }
+    public enum ResponseStatus
+    {
+        Error = 0,
+        Success = 1
+    }
+}
