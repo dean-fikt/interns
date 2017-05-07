f5efd28105b0c70eaece4132d064cf0a369c78c2
Lence Tanaskoska <lencetanaskoska@gmail.com>
2017-05-08 01:45:34 +0200

    test response added

---
+++ FiktFinanceApi/Models/TestResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FiktFinanceApi.Models
+{
+    public class TestResponse : BaseResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
