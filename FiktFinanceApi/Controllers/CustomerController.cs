1c62b99b26a31f85171db946ebb7b5143f90e7bc
Lence Tanaskoska <lencetanaskoska@gmail.com>
2017-05-10 00:31:03 +0200

    Web Appi Controllers Currency - Invoice

---
+++ FiktFinanceApi/Controllers/CustomerController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Web.Http;
+using FiktFinanceApi.Models;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.Generic;
+
+namespace FiktFinanceApi.Controllers
+{
+    public class CustomerController : BaseController
+    {
+        [HttpGet]
+        public List<Customer> GetData()
+        {
+            var conn = ConfigurationManager.ConnectionStrings[GetConnectionStringName()].ConnectionString;
+            var customerList = new List<Customer>();
+            var response = new Customer
+            {
+                Status = ResponseStatus.Success,
+            };
+            try
+            {
+                using (var con = new SqlConnection(conn))
+                {
+                    var command = new SqlCommand("USP_Custumer_Select", con) { CommandType = CommandType.StoredProcedure };
+                    con.Open();
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var customer = new Customer
+                        {
+                            Id = Convert.ToInt32(reader["IDCustumer"]),
+                            Name = reader["Name"].ToString(),
+                        };
+                        response.Id = customer.Id;
+                        response.Name = customer.Name;
+                        customerList.Add(response);
+                    }
+                    con.Close();
+                }
+                return customerList;
+            }
+            catch (Exception ex)
+            {
+                response.Status = ResponseStatus.Error;
+                response.Message = "RequestFailed";
+                customerList.Add(response);
+                return customerList;
+            }
+        }
+    }
+}
---
+++ FiktFinanceApi/Controllers/DocumentController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Web.Http;
+using FiktFinanceApi.Models;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.Generic;
+
+namespace FiktFinanceApi.Controllers
+{
+    public class DocumentController : BaseController
+    {
+        [HttpGet]
+        public List<Document> GetData()
+        {
+            var conn = ConfigurationManager.ConnectionStrings[GetConnectionStringName()].ConnectionString;
+            var documentList = new List<Document>();
+            var response = new Document
+            {
+                Status = ResponseStatus.Success,
+            };
+            try
+            {
+                using (var con = new SqlConnection(conn))
+                {
+                    var command = new SqlCommand("USP_Document_Select", con) { CommandType = CommandType.StoredProcedure };
+                    con.Open();
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var document = new Document
+                        {
+                            Id = Convert.ToInt32(reader["IDDocument"]),
+                            Name = reader["DocumentName"].ToString(),
+                            IdDocumentType = Convert.ToInt32(reader["IDDocumentType"])
+                        };
+                        response.Id = document.Id;
+                        response.Name = document.Name;
+                        response.IdDocumentType = document.IdDocumentType;
+                        documentList.Add(response);
+                    }
+                    con.Close();
+                }
+                return documentList;
+            }
+            catch (Exception ex)
+            {
+                response.Status = ResponseStatus.Error;
+                response.Message = "RequestFailed";
+                documentList.Add(response);
+                return documentList;
+            }
+        }
+    }
+}
+
