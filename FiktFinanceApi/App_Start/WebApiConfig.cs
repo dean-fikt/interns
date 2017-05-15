05675bd575bbcbbdd49aafd89b1b73d342c83cee
Lence Tanaskoska <lencetanaskoska@gmail.com>
2017-05-15 23:10:35 +0200

    set api to return json instead of xml

--- FiktFinanceApi/App_Start/WebApiConfig.cs
+++ FiktFinanceApi/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace FiktFinanceApi
@@ -20,7 +21,10 @@ namespace FiktFinanceApi
                 routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-           
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+            .Add(new MediaTypeHeaderValue("text/html"));
+
         }
     }
 }
