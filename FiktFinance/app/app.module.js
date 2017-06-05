5d6471c780b23d306b07d5a6f64885bf7fd71183
dean.jakovlevski <dean.jakovlevski@seavus.com>
2017-06-05 13:34:43 +0200

    fixed problem with main module and commented script injection

--- FiktFinance/app/app.module.js
+++ FiktFinance/app/app.module.js
@@ -1,1 +1,14 @@
 ﻿
+(function () {
+    'use strict';
+    /**
+    * @ngdoc overview
+    * @name app
+    * @description
+    * The main application module
+    */
+    var appModule = angular.module('app', [
+        
+    ]);
+   
+}());
